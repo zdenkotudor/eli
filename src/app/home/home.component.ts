ba202b09e6c68c8750d0505901b5c9ac6db62d94
Zdenko Tudor <zdenko.tudor@gmail.com>
2017-01-17 20:38:38 +0100

    More cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Title } from './title/title.service';
 @Component({
   selector: 'home',
   providers: [Title],
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
